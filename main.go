7c7f9deffcca3116eb46955062965deb7f2a2d23
Hajime Hoshi <hajimehoshi@gmail.com>
2016-02-18 00:50:05 +0900

    1+1

--- main.go
+++ main.go
@@ -3,26 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/robertkrimen/otto"
 )
 
-const src = `
-var div = document.createElement('div');
-div.textContent = 'Hello, Otto!';
-document.body.appendChild(div);`
+const src = `1+1`
 
 func main() {
 	flag.Parse()
 
-	err := func() error {
-		vm := otto.New()
-		vm.Set("document", js.Global.Get("document"))
-		_, err := vm.Run(src)
-		return err
-	}()
+	vm := otto.New()
+	result, err := vm.Run(src)
 	if err != nil {
 		switch err := err.(type) {
 		case *otto.Error:
@@ -30,6 +22,8 @@ func main() {
 		default:
 			fmt.Println(err)
 		}
-		os.Exit(64)
+		return
 	}
+	ans := js.Global.Get("document").Call("getElementById", "answer")
+	ans.Set("textContent", result)
 }
